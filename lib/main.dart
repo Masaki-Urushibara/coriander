3d312a8bdaaf41b2b0a8a6b7bf1308c78e71ff0f
Masaki-Urushibara <urushibara@trial-net.co.jp>
2021-04-19 23:37:36 +0900

    providerパターンでモデルを作ってみる

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,46 @@
+import 'package:coriander/main_model.dart';
 import 'package:coriander/next_page.dart';
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
 void main() {runApp(MyApp());}
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final items =['KBOY 1','KBOY 2','KBOY 3'];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('KBOYのflutter大学！'),
-      ),
-      body: Container(
-        width: double.infinity,
-        child: ListView(
-          scrollDirection: Axis.vertical,
-          children: <Widget>[
-            Container(
-              width: 160.0,
-              height: 80,
-              color: Colors.red,
+        title: 'Flutter Demo',
+        home: ChangeNotifierProvider<MainModel>(
+          create: (_) => MainModel(),
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text('コリアンダー'),
             ),
-            Container(
-              width: 160.0,
-              height: 80,
-              color: Colors.orange,
+            body: Consumer<MainModel>(
+              builder: (context, model, child) {
+                return Center(
+                  child: Column(
+                    children: [
+                      Text(
+                            model.kboyText,
+                        style: TextStyle(
+                          fontSize: 30
+                        ),
+                          ),
+                      RaisedButton(
+                        child: Text('ボタン'),
+                        onPressed: (){
+                          //ここで何かする
+                          model.changeKboyText();
+                        },
+                      )
+                    ],
+                  ),
+                );
+              }
             ),
-          ],
+          ),
         ),
-      ),
     );
   }
 }
